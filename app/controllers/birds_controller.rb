24a14642523fbc933f875354f4a7943ee42c445e
Troy <troyakins99@yahoo.com>
2021-09-09 12:31:09 -0500

    commit tree 9bbed4e86f5bb0499f12ae5a9716b20d7ed4c153

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -9,8 +9,10 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(bird_params)
+    bird = Bird.create!(bird_params)
     render json: bird, status: :created
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   # GET /birds/:id
@@ -22,8 +24,10 @@ class BirdsController < ApplicationController
   # PATCH /birds/:id
   def update
     bird = find_bird
-    bird.update(bird_params)
+    bird.update!(bird_params)
     render json: bird
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   # DELETE /birds/:id
